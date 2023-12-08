f8cf4a5ab47c22dfc7d0c3fe0b71b291b6efc72a
maksnowak <59841779+maksnowak@users.noreply.github.com>
2023-12-08 16:00:54 +0100

    Add login service

--- foodAppliaction/src/main/java/pap2023z/z09/accounts/AccountsTest.java
+++ foodAppliaction/src/main/java/pap2023z/z09/accounts/AccountsTest.java
@@ -16,5 +16,19 @@ public class AccountsTest {
         System.out.println(dto.getType());
         System.out.println(dto.getName());
         System.out.println(dto.getSurname());
+
+        System.out.println("Login test");
+        LoginService loginService = new LoginService();
+        System.out.println(loginService.login("admin@example.com", "admin"));
+        System.out.println(loginService.login("admin@example.com", "wrong"));
+        System.out.println(loginService.login("not_admin@example.com", "admin"));
+        System.out.println("DTO conversion test");
+        AccountsDTO dto2 = loginService.convertToDTO(accountDAO.getAccountByEmail("admin@example.com"));
+        System.out.println(dto2.getAccountId());
+        System.out.println(dto2.getEmail());
+        System.out.println(dto2.getPassword());
+        System.out.println(dto2.getType());
+        System.out.println(dto2.getName());
+        System.out.println(dto2.getSurname());
     }
 }
