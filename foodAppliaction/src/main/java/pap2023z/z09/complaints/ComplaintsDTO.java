faf8abd984cac5553bc1f30bb8ab2272deaf1010
brzoza6538 <m3.brzezinski@gmail.com>
2023-12-10 23:57:43 +0100

    initial commit - DOA and DOT files for discounts, ordered dishes, orders and payment method

---
+++ foodAppliaction/src/main/java/pap2023z/z09/complaints/ComplaintsDTO.java
@@ -0,0 +1,68 @@
+package pap2023z.z09.complaints;
+
+import pap2023z.z09.database.ComplaintsEntity;
+
+public class ComplaintsDTO {
+    private int complaintId;
+    private int orderId;
+    private String description;
+    private boolean isOpen;
+
+    public ComplaintsDTO(int complaintId, int orderId, String description, boolean isOpen){
+        this.complaintId = complaintId;
+        this.orderId = orderId;
+        this.description = description;
+        this.isOpen = isOpen;
+    }
+
+    public ComplaintsDTO() {
+
+    }
+
+        //getters
+
+    public int getComplaintId() {
+        return complaintId;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isOpen() {
+        return isOpen;
+    }
+
+    //setters
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public void setComplaintId(int complaintId) {
+        this.complaintId = complaintId;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setOpen(boolean isOpen) {
+        this.isOpen = isOpen;
+    }
+
+
+    public static ComplaintsDTO fromEntity(ComplaintsEntity entity) {
+        return new ComplaintsDTO(
+            entity.getComplaintId(),
+            entity.getOrderId(),
+            entity.getDescription(),
+            entity.isOpen()
+        );
+    }
+}
+
---
+++ foodAppliaction/src/main/java/pap2023z/z09/paymentMethods/PaymentMethodsDTO.java
@@ -0,0 +1,69 @@
+package pap2023z.z09.paymentMethods;
+
+
+import pap2023z.z09.database.OrderedDishesEntity;
+import pap2023z.z09.database.PaymentMethodsEntity;
+
+import java.sql.Date;
+
+public class PaymentMethodsDTO {
+
+    //getters
+    private int cardNumber;
+    private Date expiryDate;
+    private int cvv;
+    private int customer;
+
+    public PaymentMethodsDTO(int cardNumber, Date expiryDate, int cvv, int customer)
+    {
+        this.cardNumber = cardNumber;
+        this.expiryDate = expiryDate;
+        this.cvv = cvv;
+        this.customer = customer;
+    }
+
+    public PaymentMethodsDTO(){
+
+    }
+    public int getCardNumber(){
+        return cardNumber;
+    }
+
+    public Date getExpiryDate(){
+        return expiryDate;
+    }
+
+    public int getCvv(){
+        return cvv;
+    }
+
+    public int getCustomer()
+    {
+        return customer;
+    }
+    //setters
+    public void setCvv(int cvv){
+        this.cvv = cvv;
+    }
+
+    public void setCustomer(int customer){
+        this.customer = customer;
+    }
+
+    public void setCardNumber(int cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public void setExpiryDate(Date expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public static PaymentMethodsDTO fromEntity(PaymentMethodsEntity entity) {
+        return new PaymentMethodsDTO(
+                entity.getCardNumber(),
+                entity.getExpiryDate(),
+                entity.getCvv(),
+                entity.getCustomer()
+        );
+    }
+}
