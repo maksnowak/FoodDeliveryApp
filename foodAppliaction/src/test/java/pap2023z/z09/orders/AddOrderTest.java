616f978fa880192fc469e533c1df4a86e5fac567
brzoza6538 <m3.brzezinski@gmail.com>
2024-01-04 02:21:20 +0100

    small AddOrderTest changes

--- foodAppliaction/src/test/java/pap2023z/z09/orders/AddOrderTest.java
+++ foodAppliaction/src/test/java/pap2023z/z09/orders/AddOrderTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.math.BigDecimal;
@@ -12,7 +11,6 @@ import java.math.BigDecimal;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class AddOrderTest {
-    private static int exampleNoExistentId = -1;
     private static int exampleNoExistentAmount = -1;
     private static BigDecimal exampleNoExistentBigAmount = new BigDecimal(-1);
 
@@ -24,24 +22,9 @@ public class AddOrderTest {
         MockitoAnnotations.openMocks(this);
     }
     @Test
-    public void checkDiscountIdTest(){
-        assertThrows(IllegalArgumentException.class, () -> order.checkDiscountId(exampleNoExistentId));
-    }
-
-    @Test
-    public void checkCustomerIdTest(){
-        assertThrows(IllegalArgumentException.class, () -> order.checkCustomerId(exampleNoExistentId));
-    }
-
-    @Test
-    public void checkPaymentMethodIdTest(){
-        assertThrows(IllegalArgumentException.class, () -> order.checkPaymentMethodId(exampleNoExistentId));
-    }
-    @Test
     public void checkRange(){
         assertThrows(IllegalArgumentException.class, () -> order.checkIfInRange(exampleNoExistentAmount));
         assertThrows(IllegalArgumentException.class, () -> order.checkIfInRange(exampleNoExistentBigAmount));
-
     }
 
 }
