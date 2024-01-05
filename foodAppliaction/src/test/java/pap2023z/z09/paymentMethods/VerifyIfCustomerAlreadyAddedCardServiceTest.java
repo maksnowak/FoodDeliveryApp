f5fd89add3f7dbc2e889ee23989bef7edc58c489
maksnowak <59841779+maksnowak@users.noreply.github.com>
2024-01-05 18:02:23 +0100

    Add part of verifying if customer already has card added

---
+++ foodAppliaction/src/test/java/pap2023z/z09/paymentMethods/VerifyIfCustomerAlreadyAddedCardServiceTest.java
@@ -0,0 +1,48 @@
+package pap2023z.z09.paymentMethods;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import pap2023z.z09.database.PaymentMethodsEntity;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class VerifyIfCustomerAlreadyAddedCardServiceTest {
+    @InjectMocks
+    private VerifyIfCustomerAlreadyAddedCardService verifyIfCustomerAlreadyAddedCardService;
+
+    @Mock
+    private PaymentMethodsDAO paymentMethodsDAO;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void verifyIfCustomerAlreadyAddedCardSuccessTest() throws CardAlreadyAddedException {
+        when(paymentMethodsDAO.getAllMethods()).thenReturn(List.of());
+        LocalDate localDate = LocalDate.of(2024, 12, 31);
+        PaymentMethodsDTO dto = new PaymentMethodsDTO(1, "1234567890123456", Date.valueOf(localDate), "123", 1);
+        verifyIfCustomerAlreadyAddedCardService.verifyIfCustomerAlreadyAddedCard(dto);
+    }
+
+    @Test
+    public void verifyIfCustomerAlreadyAddedCardThrowsExceptionTest() throws CardAlreadyAddedException {
+        LocalDate localDate = LocalDate.of(2024, 12, 31);
+        PaymentMethodsDTO dto = new PaymentMethodsDTO(1, "1234567890123456", Date.valueOf(localDate), "123", 1);
+        PaymentMethodsEntity paymentMethodsEntity = new PaymentMethodsEntity();
+        paymentMethodsEntity.setCardNumber("1234567890123456");
+        paymentMethodsEntity.setCustomer(1);
+        when(paymentMethodsDAO.getAllMethods()).thenReturn(List.of(paymentMethodsEntity));
+        assertThrows(CardAlreadyAddedException.class, () -> verifyIfCustomerAlreadyAddedCardService.verifyIfCustomerAlreadyAddedCard(dto));
+    }
+
+}
