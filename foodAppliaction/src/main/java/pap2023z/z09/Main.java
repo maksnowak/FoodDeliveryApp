4e74d24261cddfd3990f887426c247da1458aa25
brzoza6538 <m3.brzezinski@gmail.com>
2024-01-06 15:16:19 +0100

    small changes

--- foodAppliaction/src/main/java/pap2023z/z09/Main.java
+++ foodAppliaction/src/main/java/pap2023z/z09/Main.java
@@ -96,57 +96,6 @@ public class Main {
             System.out.println("ID:" + OD.getId() + "  DishID:" + OD.getDishId() + "  orderID:" + OD.getOrderId());
         }
 
-
-/*
-        /////////////////crete basket
-
-        OrderedDishesDAO ODD = new OrderedDishesDAO();
-        DishesDAO DD = new DishesDAO();
-
-        Basket basket = new Basket(ODD);
-
-        basket.addItem(DD.getAllDishes().get(1).getDishId());
-        basket.addItem(DD.getAllDishes().get(2).getDishId());
-        basket.addItem(DD.getAllDishes().get(3).getDishId());
-
-        System.out.println("\n\nBAsket before and after remove:");
-
-        System.out.println(basket.getAllDishes());
-        basket.removeByIndex(1);
-        System.out.println(basket.getAllDishes());
-
-        //create order from gathered data -begin
-
-        OrdersDAO OD = new OrdersDAO();
-        OrdersDTO order = new OrdersDTO();
-        AddOrder addOrder = new AddOrder(OD);
-
-        order.setCustomerId(1);
-        order.setTotal(new BigDecimal(0));
-        order.setPaymentMethodId(1);
-        order.setStreet("ulica");
-        order.setStreetNumber(1);
-        order.setApartment(1);
-        order.setCity("miasto");
-        order.setDiscountId(1);
-        order.setStatusId(1);
-        order.setTip(new BigDecimal(0));
-
-        int orderID = (addOrder.addOrder(order));
-        //create order from gathered data -end
-
-        //use created order to link it with a basket
-        basket.basketReady(orderID);
-
-        System.out.println("\ninside orderedDishes Database\n");
-
-        List<OrderedDishesEntity> dishes = session.createQuery("from OrderedDishesEntity ").list();
-        for (OrderedDishesEntity dish : dishes) {
-            System.out.println("ID:" + dish.getId() + "  DishID:" + dish.getDishId() + "  orderID:" + dish.getOrderId());
-        }
-
- */
-
         transaction.commit();
         session.close();
         sessionFactory.close();
