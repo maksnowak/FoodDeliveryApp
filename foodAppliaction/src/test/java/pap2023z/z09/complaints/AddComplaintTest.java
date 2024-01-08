eba13332a39b6dab9fb40b9d41307f8a172c4faf
brzoza6538 <m3.brzezinski@gmail.com>
2024-01-08 16:28:55 +0100

    code cleanup

--- foodAppliaction/src/test/java/pap2023z/z09/complaints/AddComplaintTest.java
+++ foodAppliaction/src/test/java/pap2023z/z09/complaints/AddComplaintTest.java
@@ -4,9 +4,6 @@ import org.junit.jupiter.api.Test;
 
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
-import pap2023z.z09.discounts.AddDiscount;
-
-import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 public class AddComplaintTest {
@@ -28,28 +25,3 @@ public class AddComplaintTest {
     }
 
 }
-
-/*
-
-
-public class AddDiscountTest {
-
-
-    private final static int exampleBadInt = -1;
-    private final static int exampleInt = 0;
-    private final static String exampleString = "review";
-    private final static String exampleBadString = "";
-
-    @Test
-    public void checkDiscountRange(){
-        assertThrows(IllegalArgumentException.class, () -> adder.checkIfInRange(new BigDecimal(exampleBadInt)));
-        assertDoesNotThrow(() -> adder.checkIfInRange(new BigDecimal(exampleInt)));
-    }
-    @Test
-    public void checkCode(){
-        assertThrows(IllegalArgumentException.class, () -> adder.checkCode(exampleBadString));
-        assertDoesNotThrow(() -> adder.checkCode(exampleString));
-    }
-}
-
- */
