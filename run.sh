23b0d07cb3c394cd954ca473edd5d8519beb6577
maksnowak <59841779+maksnowak@users.noreply.github.com>
2024-01-09 17:16:15 +0100

    Add checking if container already exists

--- run.sh
+++ run.sh
@@ -1,10 +1,23 @@
 # Uruchomienie bazy danych w kontenerze Dockera oraz załadowanie danych początkowych
 
-docker run -p 5432:5432 --name papdb -e POSTGRES_PASSWORD=admin -d postgres
-docker cp init.sql papdb:/docker-entrypoint-initdb.d/init.sql
+# Sprawdź czy istnieje kontener o nazwie papdb
+if [ "$(docker ps | grep papdb)" ]; then
+    # Jeśli istnieje to go uruchom
+    echo "Kontener papdb już istnieje, trwa uruchamianie..."
+    docker start papdb
+    echo "Kontener papdb uruchomiony"
+else
+    # Jeśli nie istnieje to go utwórz i załaduj dane początkowe
+    echo "Kontener papdb nie istnieje, trwa tworzenie..."
+    docker run -p 5432:5432 --name papdb -e POSTGRES_PASSWORD=admin -d postgres
+    echo "Kontener papdb utworzony, trwa ładowanie danych początkowych..."
+    docker cp init.sql papdb:/docker-entrypoint-initdb.d/init.sql
+    echo "Dane początkowe załadowane"
+fi
 
 # Uruchomienie aplikacji
 
+echo "Uruchamianie aplikacji..."
 cd foodAppliaction
 mvn clean install -DskipTests
 mvn exec:java -Dexec.mainClass="pap2023z.z09.gui.App"
