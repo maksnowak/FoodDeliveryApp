f7a460443bc04248ab366ab740dad2df0fbdda5d
ad03am <ak.czuprynski@gmail.com>
2024-01-10 15:15:41 +0100

    Add add discount button

--- foodAppliaction/src/main/java/pap2023z/z09/gui/ModifyRestaurantDetailsPanel.java
+++ foodAppliaction/src/main/java/pap2023z/z09/gui/ModifyRestaurantDetailsPanel.java
@@ -25,7 +25,7 @@ public class ModifyRestaurantDetailsPanel extends JPanel {
         // Dodanie tytułu
         titleLabel = new JLabel("Zmień szczegóły restauracji: ");
         add(titleLabel);
-        JPanel buttonPanel = new JPanel(new GridLayout(4, 1));
+        JPanel buttonPanel = new JPanel(new GridLayout(5, 1));
 
         // Dodanie przycisku do dodawania dań
         dishesDAO = new DishesDAO();
@@ -58,6 +58,14 @@ public class ModifyRestaurantDetailsPanel extends JPanel {
             ((App) callback).cardLayout.show(((App) callback).getContentPane(), "ChangeHours");
         });
         buttonPanel.add(changeOpensButton);
+
+        //Dodanie przycisku do dodawania kodów rabatowych
+        JButton addDiscountCodeButton = new JButton("Dodaj kod rabatowy");
+        addDiscountCodeButton.addActionListener(e -> {
+            AddDiscountPanel addDiscountPanel = new AddDiscountPanel(callback);
+            ((App) callback).add(addDiscountPanel, "AddDiscount");
+            ((App) callback).cardLayout.show(((App) callback).getContentPane(), "AddDiscount");
+        });
 
         // Dodanie przycisku powrotu
         JButton backButton = new JButton("Powrót");
